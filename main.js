b0e543b9f1363a7d4e0ea36ea1ddae2e649f794b
diogo88811 <57359149+diogo88811@users.noreply.github.com>
2020-04-15 20:00:59 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+"use strict";
+
+
+(function()
+{	
+	window.addEventListener("load", main);
+}());
+
+
+function main()
+{
+
+	window.addEventListener("message", messageHandler);
+	
+	showPage("menu");
+}
+
+
+//---- Navegação e gestão de janelas
+function showPage(pageName)
+{
+	hidePage();
+	//carregar página na frame e enviar mensagem para a página logo que esteja carregada (frameLoadHandler)
+	var frm = document.getElementsByTagName("iframe")[0];
+	frm.src = "../html/"+pageName+".html";
+
+	/*if(pageNum == totPages) //se última, esconder botão de navegação
+	{
+		var btn = document.getElementsByTagName("button")[0];
+		btn.style.visibility = "hidden";
+		btn.removeEventListener("click", btnNextPageHandler);  //remover clicks no botão de navegação		
+	}*/
+
+}
+
+function hidePage()  //não é necessário (excepto se páginas diferentes tivessem zonas de navegação diferentes)
+{
+	var frm = document.getElementsByTagName("iframe")[0];
+	frm.src = "";
+}
+
+
+
+//ADD CODE
+
+function messageHandler(ev) {
+
+	if(ev.data == "play"){
+		//showPage("jogar");
+	}
+	else if(ev.data == "ranking"){
+		//showPage("ranking");
+	}
+	else if(ev.data == "options"){
+		showPage("options");
+	}
+	else if(ev.data == "help"){
+		showPage("ajuda");
+	}
+	else if(ev.data == "credits"){
+		//showPage("creditos");
+	}
+	else if(ev.data == "exit"){
+		console.log("Fechar");
+	}
+	else if(ev.data == "back"){
+		showPage("menu");
+	}
+}
